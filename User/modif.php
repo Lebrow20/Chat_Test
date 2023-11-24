e43ae4edf18103db727efe2adbee85f11d8082a8
Lebrow20 <nyavofanilo.rabe@gmail.com>
2023-11-24 16:15:17 +0300

    modif.php de l'user modifié

--- User/modif.php
+++ User/modif.php
@@ -35,7 +35,7 @@
                     <img src="<?php echo $img ?>" width = 100 height = 100 alt="" style="--cir:#ff2770" name="image">
                     <div class="round">
                         <input type="file" id="inputFile" name="image">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-camera" viewBox="-3 -6 20 20">
+                        <svg id="sendFile" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-camera" viewBox="6 -3 20 20">
                             <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z"/>
                             <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
                         </svg>
@@ -73,6 +73,27 @@
         input.addEventListener("change",() =>{
             image.src = URL.createObjectURL(input.files[0]);
         })
+    </script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        const sendFileButton = document.getElementById('sendFile');
+        const fileInput = document.getElementById('inputFile');
+
+        sendFileButton.addEventListener('click', function() {
+            fileInput.click(); // Déclenche le clic sur l'élément input de type file
+        });
+
+        fileInput.addEventListener('change', function() {
+            // Le code pour traiter le fichier sélectionné peut être placé ici
+            console.log('Fichier sélectionné :', this.files[0]);
+        });
+    });
+
+    </script>
+    <script>
+        function reset(){
+            document.getElementById('inputFile').value="";
+        }
     </script>
 </body>
 </html>
